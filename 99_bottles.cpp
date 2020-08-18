3a07dd476a0a53d4517660ce355883984b7b0332
ChrisMCodes <34985965+ChrisMCodes@users.noreply.github.com>
2020-08-18 09:33:36 -0700

    Delete 99_bottles.cpp

--- 99_bottles.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main(){
-    for (int i=99; i >= 1; i--) {
-        std::cout << i << " bottles of beer on the wall.\n";
-        std::cout << i << " bottles of beer.\n";
-        std::cout << "Take one down and pass it around.\n";
-        std::cout << i-1 << " bottles of beer on the wall.\n\n";
-    }
-}
